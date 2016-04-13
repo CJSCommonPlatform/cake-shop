CJSCommonPlatform/microservice_framework_version_6

Refactor Envelope payload to support JsonValue

example-context/example-service/example-command/example-command-api/src/main/java/uk/gov/justice/services/example/cakeshop/command/api/MakeCakeCommandApi.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import uk.gov.justice.services.core.annotation.Handles;
 import uk.gov.justice.services.core.annotation.ServiceComponent;
 import uk.gov.justice.services.core.sender.Sender;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.inject.Inject;
 
@@ -20,9 +20,9 @@ public class MakeCakeCommandApi {
     Sender sender;
 
     @Handles("cakeshop.commands.make-cake")
-    public void handle(final Envelope envelope) {
+    public void handle(final JsonEnvelope jsonEnvelope) {
         LOGGER.info("=============> Inside make-cake Command API");
 
-        sender.send(envelope);
+        sender.send(jsonEnvelope);
     }
 }

example-context/example-service/example-command/example-command-api/src/test/java/uk/gov/justice/services/example/cakeshop/command/api/AddRecipeCommandApiTest.java
@@ -6,7 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.justice.services.core.sender.Sender;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.JsonObject;
 
@@ -23,7 +23,7 @@ public class AddRecipeCommandApiTest {
     private static final UUID RECIPE_ID = UUID.randomUUID();
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope jsonEnvelope;
 
     @Mock
     private Sender sender;
@@ -36,12 +36,12 @@ public class AddRecipeCommandApiTest {
 
     @Test
     public void shouldHandleAddRecipeCommand() throws Exception {
-        when(envelope.payload()).thenReturn(payload);
+        when(jsonEnvelope.payloadAsJsonObject()).thenReturn(payload);
         when(payload.getString(FIELD_RECIPE_ID)).thenReturn(RECIPE_ID.toString());
 
-        addRecipeCommandApi.addRecipe(envelope);
+        addRecipeCommandApi.addRecipe(jsonEnvelope);
 
-        verify(sender, times(1)).send(envelope);
+        verify(sender, times(1)).send(jsonEnvelope);
     }
 
 }

example-context/example-service/example-command/example-command-api/src/test/java/uk/gov/justice/services/example/cakeshop/command/api/MakeCakeCommandApiTest.java
@@ -6,7 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.justice.services.core.sender.Sender;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.JsonObject;
 import java.util.UUID;
@@ -21,7 +21,7 @@ public class MakeCakeCommandApiTest {
     private static final UUID RECIPE_ID = UUID.randomUUID();
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope jsonEnvelope;
 
     @Mock
     private Sender sender;
@@ -34,12 +34,12 @@ public class MakeCakeCommandApiTest {
 
     @Test
     public void shouldHandleMakeCakeCommand() throws Exception {
-        when(envelope.payload()).thenReturn(payload);
+        when(jsonEnvelope.payloadAsJsonObject()).thenReturn(payload);
         when(payload.getString(FIELD_RECIPE_ID)).thenReturn(RECIPE_ID.toString());
 
-        makeCakeCommandApi.handle(envelope);
+        makeCakeCommandApi.handle(jsonEnvelope);
 
-        verify(sender, times(1)).send(envelope);
+        verify(sender, times(1)).send(jsonEnvelope);
     }
 
 }

example-context/example-service/example-command/example-command-controller/src/test/java/uk/gov/justice/services/example/cakeshop/command/controller/CakeShopCommandControllerTest.java
@@ -6,7 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.justice.services.core.sender.Sender;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.JsonObject;
 import java.util.UUID;
@@ -22,7 +22,7 @@ public class CakeShopCommandControllerTest {
     private static final UUID RECIPE_ID = UUID.randomUUID();
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope jsonEnvelope;
 
     @Mock
     private Sender sender;
@@ -35,21 +35,21 @@ public class CakeShopCommandControllerTest {
 
     @Test
     public void shouldHandleMakeCakeCommand() throws Exception {
-        when(envelope.payload()).thenReturn(payload);
+        when(jsonEnvelope.payloadAsJsonObject()).thenReturn(payload);
         when(payload.getString(FIELD_RECIPE_ID)).thenReturn(RECIPE_ID.toString());
-        cakeShopCommandController.makeCake(envelope);
+        cakeShopCommandController.makeCake(jsonEnvelope);
 
-        verify(sender, times(1)).send(envelope);
+        verify(sender, times(1)).send(jsonEnvelope);
     }
 
     @Test
     public void shouldHandleAddRecipeCommand() throws Exception {
-        when(envelope.payload()).thenReturn(payload);
+        when(jsonEnvelope.payloadAsJsonObject()).thenReturn(payload);
         when(payload.getString(FIELD_RECIPE_ID)).thenReturn(RECIPE_ID.toString());
 
-        cakeShopCommandController.addRecipe(envelope);
+        cakeShopCommandController.addRecipe(jsonEnvelope);
 
-        verify(sender, times(1)).send(envelope);
+        verify(sender, times(1)).send(jsonEnvelope);
     }
 
 }

example-context/example-service/example-command/example-command-handler/src/test/java/uk/gov/justice/services/example/cakeshop/command/handler/AddRecipeCommandHandlerTest.java
@@ -10,8 +10,8 @@ import uk.gov.justice.services.eventsourcing.source.core.EventSource;
 import uk.gov.justice.services.eventsourcing.source.core.EventStream;
 import uk.gov.justice.services.eventsourcing.source.core.exception.EventStreamException;
 import uk.gov.justice.services.example.cakeshop.domain.event.RecipeAdded;
-import uk.gov.justice.services.messaging.DefaultEnvelope;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.DefaultJsonEnvelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -36,7 +36,7 @@ public class AddRecipeCommandHandlerTest {
     private static final String RECIPE_NAME = "Test Recipe";
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope jsonEnvelope;
 
     @Mock
     EventSource eventSource;
@@ -45,34 +45,34 @@ public class AddRecipeCommandHandlerTest {
     Enveloper enveloper;
 
     @Mock
-    Function<Object, Envelope> enveloperFunction;
+    Function<Object, JsonEnvelope> enveloperFunction;
 
     @Mock
     Stream<RecipeAdded> events;
 
     @Mock
-    Stream<Envelope> envelopes;
+    Stream<JsonEnvelope> envelopes;
 
     @InjectMocks
     private AddRecipeCommandHandler addRecipeCommandHandler;
 
     @Test
     public void shouldHandleAddRecipeCommand() throws Exception {
-        final Envelope command = createCommand();
+        final JsonEnvelope command = createCommand();
         final EventStreamStub eventStreamStub = new EventStreamStub();
 
         when(enveloper.withMetadataFrom(command)).thenReturn(enveloperFunction);
-        when(enveloperFunction.apply(anyObject())).thenReturn(envelope);
+        when(enveloperFunction.apply(anyObject())).thenReturn(jsonEnvelope);
         when(eventSource.getStreamById(RECIPE_ID)).thenReturn(eventStreamStub);
 
         addRecipeCommandHandler.addRecipe(command);
 
         assertThat(eventStreamStub.events, notNullValue());
-        assertThat(eventStreamStub.events.findFirst().get(), equalTo(envelope));
+        assertThat(eventStreamStub.events.findFirst().get(), equalTo(jsonEnvelope));
 
     }
 
-    private Envelope createCommand() {
+    private JsonEnvelope createCommand() {
         final JsonObject metadataAsJsonObject = Json.createObjectBuilder()
                 .add(ID, UUID.randomUUID().toString())
                 .add(NAME, EVENT_NAME)
@@ -84,30 +84,30 @@ public class AddRecipeCommandHandlerTest {
                 .add("ingredients", Json.createArrayBuilder().build())
                 .build();
 
-        return DefaultEnvelope.envelopeFrom(metadataFrom(metadataAsJsonObject), payloadAsJsonObject);
+        return DefaultJsonEnvelope.envelopeFrom(metadataFrom(metadataAsJsonObject), payloadAsJsonObject);
     }
 
     private class EventStreamStub implements EventStream {
 
-        private Stream<Envelope> events;
+        private Stream<JsonEnvelope> events;
 
         @Override
-        public Stream<Envelope> read() {
+        public Stream<JsonEnvelope> read() {
             return null;
         }
 
         @Override
-        public Stream<Envelope> readFrom(Long version) {
+        public Stream<JsonEnvelope> readFrom(Long version) {
             return null;
         }
 
         @Override
-        public void append(Stream<Envelope> events) throws EventStreamException {
+        public void append(Stream<JsonEnvelope> events) throws EventStreamException {
             this.events = events;
         }
 
         @Override
-        public void appendAfter(Stream<Envelope> events, Long version) throws EventStreamException {
+        public void appendAfter(Stream<JsonEnvelope> events, Long version) throws EventStreamException {
 
         }
 

example-context/example-service/example-command/example-command-handler/src/test/java/uk/gov/justice/services/example/cakeshop/command/handler/MakeCakeCommandHandlerTest.java
@@ -7,8 +7,8 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.justice.services.eventsourcing.source.core.EventSource;
 import uk.gov.justice.services.eventsourcing.source.core.EventStream;
-import uk.gov.justice.services.messaging.DefaultEnvelope;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.DefaultJsonEnvelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -26,7 +26,7 @@ public class MakeCakeCommandHandlerTest {
     private static final UUID CAKE_ID = UUID.randomUUID();
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope jsonEnvelope;
 
     @Mock
     EventSource eventSource;
@@ -41,13 +41,13 @@ public class MakeCakeCommandHandlerTest {
     public void shouldHandleMakeCakeCommand() throws Exception {
         when(eventSource.getStreamById(CAKE_ID)).thenReturn(eventStream);
 
-        Envelope envelope = createCommandEnvelope();
+        JsonEnvelope jsonEnvelope = createCommandEnvelope();
 
-        makeCakeCommandHandler.handle(envelope);
+        makeCakeCommandHandler.handle(jsonEnvelope);
 
     }
 
-    private Envelope createCommandEnvelope() {
+    private JsonEnvelope createCommandEnvelope() {
         JsonObject metadataAsJsonObject = Json.createObjectBuilder()
                 .add(ID, UUID.randomUUID().toString())
                 .add(NAME, EVENT_NAME)
@@ -57,7 +57,7 @@ public class MakeCakeCommandHandlerTest {
                 .add("cakeId", CAKE_ID.toString())
                 .build();
 
-        return DefaultEnvelope.envelopeFrom(metadataFrom(metadataAsJsonObject), payloadAsJsonObject);
+        return DefaultJsonEnvelope.envelopeFrom(metadataFrom(metadataAsJsonObject), payloadAsJsonObject);
 
     }
 

example-context/example-service/example-event/example-event-listener/src/main/java/uk/gov/justice/services/example/cakeshop/event/listener/CakeMadeEventListener.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import uk.gov.justice.services.core.annotation.Component;
 import uk.gov.justice.services.core.annotation.Handles;
 import uk.gov.justice.services.core.annotation.ServiceComponent;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 @ServiceComponent(value = Component.EVENT_LISTENER)
 public class CakeMadeEventListener {
@@ -13,7 +13,7 @@ public class CakeMadeEventListener {
     Logger logger = LoggerFactory.getLogger(CakeMadeEventListener.class);
 
     @Handles("cakeshop.events.cake-made")
-    public void handle(final Envelope envelope) {
+    public void handle(final JsonEnvelope jsonEnvelope) {
 
         logger.info("=============> Inside cake-made Event Listener");
 

example-context/example-service/example-event/example-event-listener/src/main/java/uk/gov/justice/services/example/cakeshop/event/listener/RecipeAddedEventListener.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.justice.services.core.annotation.Handles;
 import uk.gov.justice.services.core.annotation.ServiceComponent;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import static uk.gov.justice.services.core.annotation.Component.EVENT_LISTENER;
 
@@ -15,9 +15,9 @@ public class RecipeAddedEventListener {
     private static final String FIELD_RECIPE_ID = "recipeId";
 
     @Handles("cakeshop.events.recipe-added")
-    public void handle(final Envelope envelope) {
+    public void handle(final JsonEnvelope jsonEnvelope) {
 
-        LOGGER.info("=============> Inside add-recipe Event Listener. RecipeId: " + envelope.payload().getString(FIELD_RECIPE_ID));
+        LOGGER.info("=============> Inside add-recipe Event Listener. RecipeId: " + jsonEnvelope.payloadAsJsonObject().getString(FIELD_RECIPE_ID));
         LOGGER.info("===============================================================================================");
 
     }

CJSCommonPlatform/cake-shop

example-context/example-service/example-command/example-command-api/src/main/java/uk/gov/justice/services/example/cakeshop/command/api/MakeCakeCommandApi.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import uk.gov.justice.services.core.annotation.Handles;
 import uk.gov.justice.services.core.annotation.ServiceComponent;
 import uk.gov.justice.services.core.sender.Sender;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.inject.Inject;
 
@@ -20,7 +20,7 @@ public class MakeCakeCommandApi {
     Sender sender;
 
     @Handles("cakeshop.commands.make-cake")
-    public void handle(final Envelope envelope) {
+    public void handle(final JsonEnvelope envelope) {
         LOGGER.info("=============> Inside make-cake Command API");
 
         sender.send(envelope);

example-context/example-service/example-command/example-command-api/src/test/java/uk/gov/justice/services/example/cakeshop/command/api/AddRecipeCommandApiTest.java
@@ -6,7 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.justice.services.core.sender.Sender;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.JsonObject;
 
@@ -23,7 +23,7 @@ public class AddRecipeCommandApiTest {
     private static final UUID RECIPE_ID = UUID.randomUUID();
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope envelope;
 
     @Mock
     private Sender sender;
@@ -36,7 +36,7 @@ public class AddRecipeCommandApiTest {
 
     @Test
     public void shouldHandleAddRecipeCommand() throws Exception {
-        when(envelope.payload()).thenReturn(payload);
+        when(envelope.payloadAsJsonObject()).thenReturn(payload);
         when(payload.getString(FIELD_RECIPE_ID)).thenReturn(RECIPE_ID.toString());
 
         addRecipeCommandApi.addRecipe(envelope);

example-context/example-service/example-command/example-command-api/src/test/java/uk/gov/justice/services/example/cakeshop/command/api/MakeCakeCommandApiTest.java
@@ -6,7 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.justice.services.core.sender.Sender;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.JsonObject;
 import java.util.UUID;
@@ -21,7 +21,7 @@ public class MakeCakeCommandApiTest {
     private static final UUID RECIPE_ID = UUID.randomUUID();
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope envelope;
 
     @Mock
     private Sender sender;
@@ -34,7 +34,7 @@ public class MakeCakeCommandApiTest {
 
     @Test
     public void shouldHandleMakeCakeCommand() throws Exception {
-        when(envelope.payload()).thenReturn(payload);
+        when(envelope.payloadAsJsonObject()).thenReturn(payload);
         when(payload.getString(FIELD_RECIPE_ID)).thenReturn(RECIPE_ID.toString());
 
         makeCakeCommandApi.handle(envelope);

example-context/example-service/example-command/example-command-controller/src/test/java/uk/gov/justice/services/example/cakeshop/command/controller/CakeShopCommandControllerTest.java
@@ -6,7 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.justice.services.core.sender.Sender;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.JsonObject;
 import java.util.UUID;
@@ -22,7 +22,7 @@ public class CakeShopCommandControllerTest {
     private static final UUID RECIPE_ID = UUID.randomUUID();
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope envelope;
 
     @Mock
     private Sender sender;
@@ -35,7 +35,7 @@ public class CakeShopCommandControllerTest {
 
     @Test
     public void shouldHandleMakeCakeCommand() throws Exception {
-        when(envelope.payload()).thenReturn(payload);
+        when(envelope.payloadAsJsonObject()).thenReturn(payload);
         when(payload.getString(FIELD_RECIPE_ID)).thenReturn(RECIPE_ID.toString());
         cakeShopCommandController.makeCake(envelope);
 
@@ -44,7 +44,7 @@ public class CakeShopCommandControllerTest {
 
     @Test
     public void shouldHandleAddRecipeCommand() throws Exception {
-        when(envelope.payload()).thenReturn(payload);
+        when(envelope.payloadAsJsonObject()).thenReturn(payload);
         when(payload.getString(FIELD_RECIPE_ID)).thenReturn(RECIPE_ID.toString());
 
         cakeShopCommandController.addRecipe(envelope);

example-context/example-service/example-command/example-command-handler/src/test/java/uk/gov/justice/services/example/cakeshop/command/handler/AddRecipeCommandHandlerTest.java
@@ -10,8 +10,8 @@ import uk.gov.justice.services.eventsourcing.source.core.EventSource;
 import uk.gov.justice.services.eventsourcing.source.core.EventStream;
 import uk.gov.justice.services.eventsourcing.source.core.exception.EventStreamException;
 import uk.gov.justice.services.example.cakeshop.domain.event.RecipeAdded;
-import uk.gov.justice.services.messaging.DefaultEnvelope;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.DefaultJsonEnvelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -36,7 +36,7 @@ public class AddRecipeCommandHandlerTest {
     private static final String RECIPE_NAME = "Test Recipe";
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope envelope;
 
     @Mock
     EventSource eventSource;
@@ -45,20 +45,20 @@ public class AddRecipeCommandHandlerTest {
     Enveloper enveloper;
 
     @Mock
-    Function<Object, Envelope> enveloperFunction;
+    Function<Object, JsonEnvelope> enveloperFunction;
 
     @Mock
     Stream<RecipeAdded> events;
 
     @Mock
-    Stream<Envelope> envelopes;
+    Stream<JsonEnvelope> envelopes;
 
     @InjectMocks
     private AddRecipeCommandHandler addRecipeCommandHandler;
 
     @Test
     public void shouldHandleAddRecipeCommand() throws Exception {
-        final Envelope command = createCommand();
+        final JsonEnvelope command = createCommand();
         final EventStreamStub eventStreamStub = new EventStreamStub();
 
         when(enveloper.withMetadataFrom(command)).thenReturn(enveloperFunction);
@@ -72,7 +72,7 @@ public class AddRecipeCommandHandlerTest {
 
     }
 
-    private Envelope createCommand() {
+    private JsonEnvelope createCommand() {
         final JsonObject metadataAsJsonObject = Json.createObjectBuilder()
                 .add(ID, UUID.randomUUID().toString())
                 .add(NAME, EVENT_NAME)
@@ -84,30 +84,30 @@ public class AddRecipeCommandHandlerTest {
                 .add("ingredients", Json.createArrayBuilder().build())
                 .build();
 
-        return DefaultEnvelope.envelopeFrom(metadataFrom(metadataAsJsonObject), payloadAsJsonObject);
+        return DefaultJsonEnvelope.envelopeFrom(metadataFrom(metadataAsJsonObject), payloadAsJsonObject);
     }
 
     private class EventStreamStub implements EventStream {
 
-        private Stream<Envelope> events;
+        private Stream<JsonEnvelope> events;
 
         @Override
-        public Stream<Envelope> read() {
+        public Stream<JsonEnvelope> read() {
             return null;
         }
 
         @Override
-        public Stream<Envelope> readFrom(Long version) {
+        public Stream<JsonEnvelope> readFrom(Long version) {
             return null;
         }
 
         @Override
-        public void append(Stream<Envelope> events) throws EventStreamException {
+        public void append(Stream<JsonEnvelope> events) throws EventStreamException {
             this.events = events;
         }
 
         @Override
-        public void appendAfter(Stream<Envelope> events, Long version) throws EventStreamException {
+        public void appendAfter(Stream<JsonEnvelope> events, Long version) throws EventStreamException {
 
         }
 

example-context/example-service/example-command/example-command-handler/src/test/java/uk/gov/justice/services/example/cakeshop/command/handler/MakeCakeCommandHandlerTest.java
@@ -7,8 +7,8 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.justice.services.eventsourcing.source.core.EventSource;
 import uk.gov.justice.services.eventsourcing.source.core.EventStream;
-import uk.gov.justice.services.messaging.DefaultEnvelope;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.DefaultJsonEnvelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -26,7 +26,7 @@ public class MakeCakeCommandHandlerTest {
     private static final UUID CAKE_ID = UUID.randomUUID();
 
     @Mock
-    Envelope envelope;
+    JsonEnvelope envelope;
 
     @Mock
     EventSource eventSource;
@@ -41,13 +41,13 @@ public class MakeCakeCommandHandlerTest {
     public void shouldHandleMakeCakeCommand() throws Exception {
         when(eventSource.getStreamById(CAKE_ID)).thenReturn(eventStream);
 
-        Envelope envelope = createCommandEnvelope();
+        JsonEnvelope envelope = createCommandEnvelope();
 
         makeCakeCommandHandler.handle(envelope);
 
     }
 
-    private Envelope createCommandEnvelope() {
+    private JsonEnvelope createCommandEnvelope() {
         JsonObject metadataAsJsonObject = Json.createObjectBuilder()
                 .add(ID, UUID.randomUUID().toString())
                 .add(NAME, EVENT_NAME)
@@ -57,7 +57,7 @@ public class MakeCakeCommandHandlerTest {
                 .add("cakeId", CAKE_ID.toString())
                 .build();
 
-        return DefaultEnvelope.envelopeFrom(metadataFrom(metadataAsJsonObject), payloadAsJsonObject);
+        return DefaultJsonEnvelope.envelopeFrom(metadataFrom(metadataAsJsonObject), payloadAsJsonObject);
 
     }
 

example-context/example-service/example-event/example-event-listener/src/main/java/uk/gov/justice/services/example/cakeshop/event/listener/CakeMadeEventListener.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import uk.gov.justice.services.core.annotation.Component;
 import uk.gov.justice.services.core.annotation.Handles;
 import uk.gov.justice.services.core.annotation.ServiceComponent;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 @ServiceComponent(value = Component.EVENT_LISTENER)
 public class CakeMadeEventListener {
@@ -13,7 +13,7 @@ public class CakeMadeEventListener {
     Logger logger = LoggerFactory.getLogger(CakeMadeEventListener.class);
 
     @Handles("cakeshop.events.cake-made")
-    public void handle(final Envelope envelope) {
+    public void handle(final JsonEnvelope envelope) {
 
         logger.info("=============> Inside cake-made Event Listener");
 

example-context/example-service/example-event/example-event-listener/src/main/java/uk/gov/justice/services/example/cakeshop/event/listener/RecipeAddedEventListener.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.justice.services.core.annotation.Handles;
 import uk.gov.justice.services.core.annotation.ServiceComponent;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import static uk.gov.justice.services.core.annotation.Component.EVENT_LISTENER;
 
@@ -15,9 +15,9 @@ public class RecipeAddedEventListener {
     private static final String FIELD_RECIPE_ID = "recipeId";
 
     @Handles("cakeshop.events.recipe-added")
-    public void handle(final Envelope envelope) {
+    public void handle(final JsonEnvelope envelope) {
 
-        LOGGER.info("=============> Inside add-recipe Event Listener. RecipeId: " + envelope.payload().getString(FIELD_RECIPE_ID));
+        LOGGER.info("=============> Inside add-recipe Event Listener. RecipeId: " + envelope.payloadAsJsonObject().getString(FIELD_RECIPE_ID));
         LOGGER.info("===============================================================================================");
 
     }