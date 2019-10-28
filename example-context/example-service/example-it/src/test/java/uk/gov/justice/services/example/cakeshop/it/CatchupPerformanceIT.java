750d689fef5685d7514eee3646943f3907666b26
amckenzie <allan.mckenzie@agilesphere.co.uk>
2019-10-28 11:42:15 +0000

    Update event-store to 2.2.4

--- example-context/example-service/example-it/src/test/java/uk/gov/justice/services/example/cakeshop/it/CatchupPerformanceIT.java
+++ example-context/example-service/example-it/src/test/java/uk/gov/justice/services/example/cakeshop/it/CatchupPerformanceIT.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.services.example.cakeshop.it;
 
 import static java.lang.Integer.valueOf;
+import static java.lang.String.format;
 import static java.lang.System.getProperty;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
@@ -11,11 +12,7 @@ import static uk.gov.justice.services.jmx.system.command.client.connection.JmxPa
 import static uk.gov.justice.services.test.utils.common.host.TestHostProvider.getHost;
 
 import uk.gov.justice.services.eventsourcing.repository.jdbc.event.Event;
-import uk.gov.justice.services.eventsourcing.repository.jdbc.event.EventJdbcRepository;
-import uk.gov.justice.services.eventsourcing.repository.jdbc.event.EventRepositoryFactory;
-import uk.gov.justice.services.eventsourcing.repository.jdbc.event.EventStreamJdbsRepositoryFactory;
-import uk.gov.justice.services.eventsourcing.repository.jdbc.eventstream.EventStreamJdbcRepository;
-import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidPositionException;
+import uk.gov.justice.services.example.cakeshop.it.helpers.BatchEventInserter;
 import uk.gov.justice.services.example.cakeshop.it.helpers.CakeshopEventGenerator;
 import uk.gov.justice.services.example.cakeshop.it.helpers.DatabaseManager;
 import uk.gov.justice.services.example.cakeshop.it.helpers.PositionInStreamIterator;
@@ -31,6 +28,8 @@ import uk.gov.justice.services.test.utils.persistence.DatabaseCleaner;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -43,14 +42,12 @@ import org.junit.Test;
 
 public class CatchupPerformanceIT {
 
+    private static final int BATCH_INSERT_SIZE = 10_000;
+
     private static final String CONTEXT_NAME = "example";
 
     private final DataSource eventStoreDataSource = new DatabaseManager().initEventStoreDb();
     private final DataSource viewStoreDataSource = new DatabaseManager().initViewStoreDb();
-    private final EventJdbcRepository eventJdbcRepository = new EventRepositoryFactory().getEventJdbcRepository(eventStoreDataSource);
-
-    private final EventStreamJdbsRepositoryFactory eventStreamJdbcRepositoryFactory = new EventStreamJdbsRepositoryFactory();
-    private final EventStreamJdbcRepository eventStreamJdbcRepository = eventStreamJdbcRepositoryFactory.getEventStreamJdbcRepository(eventStoreDataSource);
 
     private final ProcessedEventCounter processedEventCounter = new ProcessedEventCounter(viewStoreDataSource);
 
@@ -60,7 +57,8 @@ public class CatchupPerformanceIT {
     private final TestSystemCommanderClientFactory systemCommanderClientFactory = new TestSystemCommanderClientFactory();
     private final DatabaseCleaner databaseCleaner = new DatabaseCleaner();
 
-    private final Poller longPoller = new Poller(1200, 1000L);
+    private final Poller longPoller = new Poller(2400, 1000L);
+    private final BatchEventInserter batchEventInserter = new BatchEventInserter(eventStoreDataSource, BATCH_INSERT_SIZE);
 
     private Client client;
 
@@ -87,27 +85,23 @@ public class CatchupPerformanceIT {
         final int numberOfEventsPerStream = 100;
         final int totalEvents = numberOfStreams * numberOfEventsPerStream;
 
+        System.out.println(format(
+                "Inserting %d events into event_log (%d events in %d streams)",
+                totalEvents,
+                numberOfEventsPerStream,
+                numberOfStreams
+        ));
+
         addEventsToEventLog(numberOfStreams, numberOfEventsPerStream);
 
-        final Optional<Integer> numberOfEvents = longPoller.pollUntilFound(() -> {
-            final int eventCount = processedEventCounter.countProcessedEvents();
-            if (eventCount == totalEvents) {
-                return of(eventCount);
-            }
-
-            return empty();
-        });
-
-        if (numberOfEvents.isPresent()) {
-            System.out.println("Inserted " + numberOfEvents.get() + " events");
-        } else {
-            fail("Failed to insert " + totalEvents + " events");
-        }
-
+        System.out.println("Inserted " + totalEvents + " events into event_log");
+        System.out.println("Waiting for events to publish...");
         cleanViewstoreTables();
 
         longPoller.pollUntilFound(() -> {
             final int eventCount = processedEventCounter.countProcessedEvents();
+            System.out.println(format("%s events in processed_event table", eventCount));
+
             if (eventCount == 0) {
                 return of(eventCount);
             }
@@ -127,10 +121,13 @@ public class CatchupPerformanceIT {
             assertThat(eventCount, is(0));
         }
 
+        System.out.println("Running catchup...");
         runCatchup();
 
         final Optional<Integer> numberOfReplayedEvents = longPoller.pollUntilFound(() -> {
             final int eventCount = processedEventCounter.countProcessedEvents();
+            System.out.println(format("%s events in processed_event table", eventCount));
+
             if (eventCount == totalEvents) {
                 return of(eventCount);
             }
@@ -139,15 +136,18 @@ public class CatchupPerformanceIT {
         });
 
         if (numberOfReplayedEvents.isPresent()) {
-            System.out.println("Successfully caught up " + numberOfEvents.get() + " events");
+            System.out.println("Successfully caught up " + numberOfReplayedEvents.get() + " events");
         } else {
             fail("Failed to catchup " + totalEvents + " events.");
         }
     }
 
-    private void addEventsToEventLog(final int numberOfStreams, final int numberOfEventsPerStream) throws InvalidPositionException {
+    private void addEventsToEventLog(final int numberOfStreams, final int numberOfEventsPerStream) throws Exception {
 
         final CakeshopEventGenerator cakeshopEventGenerator = new CakeshopEventGenerator();
+
+        final List<Event> events = new ArrayList<>();
+        final List<UUID> streamIds = new ArrayList<>();
 
         for (int seed = 0; seed < numberOfStreams; seed++) {
 
@@ -156,14 +156,21 @@ public class CatchupPerformanceIT {
             final Event recipeAddedEvent = cakeshopEventGenerator.createRecipeAddedEvent(seed, positionInStreamIterator);
             final UUID recipeId = recipeAddedEvent.getStreamId();
 
-            eventStreamJdbcRepository.insert(recipeId);
-            eventJdbcRepository.insert(recipeAddedEvent);
+            if (!streamIds.contains(recipeId)) {
+                streamIds.add(recipeId);
+            }
+
+            events.add(recipeAddedEvent);
 
             for (int renameNumber = 1; renameNumber < numberOfEventsPerStream; renameNumber++) {
                 final Event recipeRenamedEvent = cakeshopEventGenerator.createRecipeRenamedEvent(recipeId, seed, renameNumber, positionInStreamIterator);
-                eventJdbcRepository.insert(recipeRenamedEvent);
+                events.add(recipeRenamedEvent);
             }
         }
+
+        batchEventInserter.updateEventStreamTable(streamIds);
+        batchEventInserter.updateEventLogTable(events);
+
     }
 
     private void runCatchup() throws Exception {
