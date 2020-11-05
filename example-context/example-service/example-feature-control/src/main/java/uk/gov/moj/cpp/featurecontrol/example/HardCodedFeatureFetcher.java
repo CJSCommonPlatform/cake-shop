662e624c404ecaac2ccdd3be4956cb32809036d0
amckenzie <allan.mckenzie@agilesphere.co.uk>
2020-11-05 10:30:41 +0000

    Stub call to azure to fetch features

---
+++ example-context/example-service/example-feature-control/src/main/java/uk/gov/moj/cpp/featurecontrol/example/HardCodedFeatureFetcher.java
@@ -0,0 +1,41 @@
+package uk.gov.moj.cpp.featurecontrol.example;
+
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+
+import uk.gov.justice.services.core.featurecontrol.FeatureFetcher;
+import uk.gov.justice.services.core.featurecontrol.domain.Feature;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+@Default
+@ApplicationScoped
+public class HardCodedFeatureFetcher implements FeatureFetcher {
+
+    private static final Feature HARD_CODED_DISABLED_FEATURE = new Feature(
+            "recipes-have-allergens-specified",
+            "Version 2 of add recipe handler for testing feature toggling",
+            false
+    );
+
+    @Inject
+    private Logger logger;
+
+    @Override
+    public List<Feature> fetchFeatures() {
+        final List<Feature> features = singletonList(HARD_CODED_DISABLED_FEATURE);
+
+        final List<String> featureNames = features.stream().map(Feature::getFeatureName).collect(toList());
+
+        logger.warn("Stubbing fetch of hard coded features: " + featureNames);
+
+        return features;
+    }
+}
